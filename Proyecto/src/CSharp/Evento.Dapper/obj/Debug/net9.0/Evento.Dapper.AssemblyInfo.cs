e5504859a4869c388507893effec8e6039c28d6b
Sebastian Zerpa <darkopx56@gmail.com>
2025-09-22 13:52:04 -0300

    nada

--- Proyecto/src/CSharp/Evento.Dapper/obj/Debug/net9.0/Evento.Dapper.AssemblyInfo.cs
+++ Proyecto/src/CSharp/Evento.Dapper/obj/Debug/net9.0/Evento.Dapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Evento.Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d420787b12208fdead0c526e5e807eb61c655e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e612920fc0a5f71bd03c8995c66b37dbb19472")]
 [assembly: System.Reflection.AssemblyProductAttribute("Evento.Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Evento.Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
